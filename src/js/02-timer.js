7dfaa4f306c7c69b227d35e0a5557faec5c976ce
Hanna Vakulenko <anna.vakulenko.ua@gmail.com>
2023-03-02 19:13:49 +0200

    2 and 3 tasks are done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,5 @@
+// Завдання 2 - таймер зворотного відліку
+
 // Імпорт бібліотеки flatpickr
 import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
@@ -5,32 +7,97 @@ import 'flatpickr/dist/flatpickr.min.css';
 // Імпорт бібліотеки notiflix
 import Notiflix from 'notiflix';
 
-//Бібліотека очікує, що її ініціалізують на елементі input[type="text"],
-// тому ми додали до HTML документу поле input#datetime - picker
-//Другим аргументом функції flatpickr(selector, options) можна передати необов'язковий об'єкт параметрів.
+// Отримання посилань на елементи дому
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+  timer: document.querySelector('.timer'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
+
+// Встановлюємо початковий стан кнопки неактивним
+refs.startBtn.disabled = true;
+
+// Налаштування об'єкту параметрів бібліотеки flatpickr
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+  // Метод викликається щоразу під час закриття елемента інтерфейсу, який створює flatpickr
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    // Порівнюємо перший елемент з масиву обраних дат. Якщо користувач вибрав дату в минулому,
+    // показуємо повідомлення "Please choose a date in the future"
+    if (selectedDates[0] < new Date()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      // Кнопка «Start» повинна бути неактивною доти, доки користувач не вибрав дату в майбутньому.
+      refs.startBtn.disabled = true;
+    } else {
+      refs.startBtn.disabled = false;
+    }
   },
 };
 
-const refs = {
-  input: document.querySelector('input[type="text"]'),
-  startBtn: document.querySelector('button[data-start]'),
-  days: document.querySelector('span[data-days]'),
-  hours: document.querySelector('span[data-hours]'),
-  minuts: document.querySelector('span[data-minutes]'),
-  seconds: document.querySelector('span[data-seconds]'),
+// Виклик функції для вибору дати та часу в елементі input
+flatpickr(refs.input, options);
+
+// Функція, що повертає об'єкт з розрахованим часом, що залишився до кінцевої дати.
+const convertMs = ms => {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
 };
-// Встановлюємо початковий стан кнопки неактивним
-refs.startBtn.disabled = true;
 
-flatpickr('#datetime-picker', options);
+// Функцію, яка використовує метод padStart() для форматування результату ф-ції convertMs().
+const addLeadingZero = value => {
+  return value.toString().padStart(2, '0');
+};
 
-// input.onClose(selectedDates) {
-//     window.alert("Please choose a date in the future");
-// };
+// Функція, що запускає відлік часу
+const onHandleClick = () => {
+  // Запуск зворотнього відліку з інтервалом 1000 (1 секунда)
+  let countdown = setInterval(() => {
+    // Визначення часу, скільки залишилось до вказаної дати
+    let differenceOfDates = new Date(refs.input.value) - new Date();
+
+    // старт зворотнього відліку за умови, що різниця часу до кінцевої дати більше ніж нуль
+    if (differenceOfDates >= 0) {
+      // деструктурізація об'єкту з розрахованим часом по дням, годинам, мінутам та секундам (функція конвертації мілісекунд в об'єкт значень)
+      let { days, hours, minutes, seconds } = convertMs(differenceOfDates);
+
+      refs.days.textContent = addLeadingZero(days);
+      refs.hours.textContent = addLeadingZero(hours);
+      refs.minutes.textContent = addLeadingZero(minutes);
+      refs.seconds.textContent = addLeadingZero(seconds);
+      // деактивація кнопки "Старт"
+      refs.startBtn.disabled = true;
+      //зміна коліру тексту, коли почався зворотній виклик
+      refs.timer.style.color = 'red';
+    } else {
+      Notiflix.Notify.success('Time is up!');
+      //зупинка зворотнього відліку, коли до кінцевої дати, тобто 00:00:00:00
+      clearInterval(countdown);
+      //повернення коліру тексту до початкового
+      refs.timer.style.color = 'black';
+    }
+  }, 1000);
+};
+
+// Додаємо слухача на натискання кнопки Start
+refs.startBtn.addEventListener('click', onHandleClick);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,62 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+// Завдання 3 - генератор промісів
+// Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay)
+// стільки разів, скільки ввели в поле amount. Під час кожного виклику передай їй номер промісу(position),
+// що створюється, і затримку, враховуючи першу затримку(delay), введену користувачем, і крок(step).
+
+// Імпорт бібліотеки notiflix
+import Notiflix from 'notiflix';
+
+// Отримання посилань на елементи дому
+const refs = {
+  form: document.querySelector('.form'),
+  inputDelay: document.querySelector('input[name="delay"]'),
+  inputStep: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+  createPromiseBtn: document.querySelector('button[type="submit"]'),
+};
+
+//Функція створення промісу
+const createPromise = (position, delay) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
+};
+
+// Функція обробки події
+const handleOnClick = e => {
+  e.preventDefault(); // відміна оновлення сторінки за замовчуванням при натисканні кнопки з типом "Submit"
+
+  // Обробка введених даних в інпутах
+  // Отримання числових значень першої затримки в мілісекундах та кроку збільшення затримки для кожного промісу
+  let firstDelay = Number(refs.inputDelay.value);
+  let delayStep = Number(refs.inputStep.value);
+
+  // Створення циклу, який перебирає по введеній користувачем кількості проміси, що створюються
+  for (let i = 0; i < refs.amount.value; i++) {
+    // Виклик функції створення промісів стільки разів, скільки введено в поле amount.
+    // Після першої затримки кожна наступна буде збільшуватися на введений користувачем крок
+    createPromise(1 + i, firstDelay + i * delayStep)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
-}
+  refs.form.reset(); //очищення полів форми
+};
+
+// Додавання слухача на клік на кнопку
+refs.createPromiseBtn.addEventListener('click', handleOnClick);
